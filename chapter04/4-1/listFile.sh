28702772a37f7efc50d8c92b5bcd2a9e0e3d7698
pengpeng.wang <pengpeng.wang@analyticservice.net>
2019-10-29 10:40:40 +0800

    ls 指令和参数

---
+++ chapter04/4-1/listFile.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# ls
+#-a, --all List all entries including ones that start with a dot   全部包含  . ..
+#-A, --almost-all List all entries excluding . and .. 全部 不包含  . ..
+#-c Sort files by change time 按修改事件排序
+#-d, --directory List directory entries 显示文件夹
+#-h, --human-readable Show sizes in human readable format (i.e. K, M) 显示可读大小
+#-H Same as above only with powers of 1000 instead of 1024
+#-l Show contents in long-listing format 完整格式
+#-o Long -listing format without group info 去除分组信息
+#-r, --reverse Show contents in reverse order 倒序
+#-s, --size Print size of each file in blocks 显示尺寸
+#-S Sort by file size 按照大小排序
+#--sort=WORD Sort contents by a word. (i.e size, version, status)  按照关键字排序
+#-t Sort by modification time 按照修改时间排序
+#-u Sort by last access time 按照最后访问时间排序
+#-v Sort by version 按照版本排序
+#-1 List one file per line 一行一个文件
