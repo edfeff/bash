448c746e356086a39d56098bcbcfdea3c94cb352
pengpeng.wang <pengpeng.wang@analyticservice.net>
2019-10-29 10:17:37 +0800

    #！ shebang 对脚本执行的影响

---
+++ chapter02/1-2/python.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/python
+print(1,2,3)
+#使用python 的shebang，可以执行Python的脚本,不过使用sh执行时则会出错。
---
+++ chapter02/1-2/somthingwrong.sh
@@ -0,0 +1,7 @@
+#!/bin/bash something wrong
+echo "something wrong"
+# 使用 sh 或者执行 bash执行此脚本时，不会由错误发生
+#如果给此文件赋予执行权限
+# chmod +x xxx.sh
+# 在 ./xxx.sh 执行时，则会出现错误
+# /bin/bash: something wrong: No such file or directory
